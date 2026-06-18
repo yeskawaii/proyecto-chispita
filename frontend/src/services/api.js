9f8b40f3462dd8b259ce7b26476b96f34b17832f
Julio Alejandro Yescas Tinoco <yescas@MacBook-Air-de-Julio.local>
2026-06-18 12:22:35 -0600

    feat: adding the dockefiles for the server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://api-chispita.duckdns.org';
 
 export const getViajes = async () => {
   const res = await fetch(`${API_URL}/viajes/`);
